55edc231de15fd191ac2daabdfb2f626c6bc9e77
pewerner <pewerner@comcast.net>
2017-01-23 14:51:20 -0500

    nother commit

---
+++ app/app.js
@@ -0,0 +1,102 @@
+﻿//--App.js
+var bionexApp = angular.module('bionexApp', [
+  'ngRoute', 'ngTouch',
+  'bionexControllers', "ngSanitize"
+]);
+
+bionexApp.config(['$routeProvider', '$locationProvider',
+  function ($routeProvider, $locationProvider) {
+      $routeProvider.
+       when('/bumblebee.html', {
+           templateUrl: 'views/bumblebee.min.html'
+           //controller: 'InstrumentCtrl'
+       }).when('/centrifuge.html', {
+           templateUrl: 'views/centrifuge.min.html'
+           //controller: 'InstrumentCtrl'
+       }).when('/systems.html', {
+           templateUrl: 'views/systems.min.html',
+           //controller: 'HomeCtrl'
+       }).
+        when('/', {
+            templateUrl: 'views/home.min.html',
+            //controller: 'MainCtrl'
+        }).
+        when('/home.html', {
+            templateUrl: 'views/home.min.html',
+            //controller: 'MainCtrl'
+        }).when('/home-alt.html', {
+                      templateUrl: 'views/home-alt.min.html',
+                      //controller: 'MainCtrl'
+                  }).
+        when('/contact.html', {
+            templateUrl: 'views/contact.min.html',
+            //controller: 'HomeCtrl'
+        }).
+       when('/software.html', {
+           templateUrl: 'views/software.min.html',
+           //controller: 'HomeCtrl'
+       }).when('/beesure.html', {
+           templateUrl: 'views/beesure.min.html',
+           //controller: 'HomeCtrl'
+       }).when('/bnx1536.html', {
+           templateUrl: 'views/bnx1536.min.html',
+           //controller: 'HomeCtrl'
+       }).
+       when('/nanodrop.html', {
+           templateUrl: 'views/nanodrop.min.html',
+           //controller: 'HomeCtrl'
+       }).
+       when('/stinger.html', {
+          templateUrl: 'views/stinger.min.html',
+                     //controller: 'HomeCtrl'
+       }).
+      when('/stacker.html', {
+                     templateUrl: 'views/stacker.min.html',
+                     //controller: 'HomeCtrl'
+                 }).
+       when('/about.html', {
+           templateUrl: 'views/about.min.html',
+           //controller: 'HomeCtrl'
+       }).
+       when('/hive.html', {
+           templateUrl: 'views/hive.min.html',
+           //controller: 'HomeCtrl'
+       }).when('/alternate', {
+           templateUrl: 'views/home-alternate.html',
+           //controller: 'HomeCtrl'
+       }).when('/partnership.html', {
+           templateUrl: 'views/partnership.min.html',
+           //controller: 'HomeCtrl'
+       }).when('/vworks.html', {
+           templateUrl: 'views/VWorks.min.html',
+           //controller: 'HomeCtrl'
+       }).when('/synapsis.html', {
+           templateUrl: 'views/Synapsis.min.html',
+           //controller: 'HomeCtrl'
+       }).when('/careers.html', {
+           templateUrl: 'views/careers.min.html',
+           //controller: 'HomeCtrl'
+       }).when('/information.html', {
+           templateUrl: 'views/information.min.html',
+           //controller: 'HomeCtrl'
+       }).when('/support.html', {
+           templateUrl: 'support.html',
+           //controller: 'HomeCtrl'
+       }).when('/slas.html', {
+           templateUrl: 'views/slas.min.html',
+           //controller: 'HomeCtrl'
+       }).otherwise({
+           redirectTo: '/home.html'
+       });
+      $locationProvider.html5Mode(true);
+
+
+
+
+      //window.location.hash = '/'
+  }]).run(function ($rootScope, $location) {
+
+
+
+  });
+
---
+++ app/controler.js
@@ -0,0 +1,133 @@
+﻿var bionexControllers = angular.module('bionexControllers', ['ngAnimate', 'ui.bootstrap']);
+function NavBarCtrl($scope) {
+
+    $scope.isCollapsed = true;
+}
+
+function adjustThumbnails() {
+
+    var tn1 = $("#thumbnail-1").height();
+    //alert(tn1)
+    var tn2 = $("#thumbnail-2").height();
+
+    var tn3 = (tn1 - tn2) - 24;
+
+    //if (tn1 > tn2) {
+    //    $("#thumbnail-1").height(tn1);
+    //    $("#thumbnail-2").height(tn1);
+    //} else {
+    //    $("#thumbnail-1").height(tn2);
+    //    $("#thumbnail-2").height(tn2);
+
+
+
+    //}
+
+
+    //$("#thumbnail-2").height(296);
+    //$("#thumbnail-3").height(296);
+
+   // $("#thumbnail-3").height(tn3);
+
+   
+
+
+}
+
+$(window).load(function () {
+    $("#page-content").show();
+    setTimeout(function () { adjustThumbnails() }, 500);
+
+
+});
+
+
+
+$(window).resize(function () {
+    adjustThumbnails();
+});
+
+
+bionexControllers.controller('MainCtrl', function ($scope, $rootScope, $location) {
+
+
+
+    //--This closes the navbar dropdown when you click  on a link, also scrolls the page to the top
+    $(".dropper").click(function () {
+
+        $("nav").removeClass("dropdown-onhover");
+        $("li.dropdown-full.open").removeClass("open");
+    });
+
+    adjustThumbnails();
+    $scope.showBackground = true;
+
+    $scope.adjustThumbnails = function () {
+
+
+        adjustThumbnails();
+
+    }
+
+    $rootScope.$on("$locationChangeStart", function (event, next, current) {
+        $("body").scrollTop(0);
+        adjustThumbnails();
+
+    });
+
+    $scope.hidebackground = function () {
+        $scope.bodyStyle = {
+            "background-image": "none"
+        }
+    }
+
+    $scope.myInterval = 4000;
+    $scope.noWrapSlides = false;
+    var slides = $scope.slides = [];
+
+    slides.push({
+        image: 'Images/sliders/laser-slider.png',
+        href: 'stinger.html'
+
+    });
+    slides.push({
+        image: 'Images/sliders/hive-slider.png',
+        href: 'hive.html'
+
+    });
+
+
+    slides.push({
+        image: 'Images/sliders/BeeSmartKart.png',
+        href: 'hive.html'
+
+    });
+    slides.push({
+        image: 'Images/sliders/bumblebee-slider.png',
+        href: 'bumblebee.html'
+    });
+    slides.push({
+        image: 'Images/sliders/hig-slider.png',
+        href: 'centrifuge.html'
+    });
+    slides.push({
+        image: 'Images/sliders/beessure-slider.png',
+        href: 'beesure.html'
+    });
+    slides.push({
+        image: 'Images/sliders/BNX1536-slider.png',
+        href: 'bnx1536.html'
+    });
+    slides.push({
+        image: 'Images/sliders/nanodrop-slider.png',
+        href: 'nanodrop.html'
+    });
+    slides.push({
+        image: 'Images/sliders/direct-drive-slider.png',
+        href: 'systems.html'
+    });
+
+
+});
+
+
